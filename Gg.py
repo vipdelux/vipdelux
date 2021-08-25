5bac88a1a78eb5abaf2c5e26f3fb11c21883ba7e
vipdelux <81058845+vipdelux@users.noreply.github.com>
2021-08-26 01:01:47 +0300

    Update Gg.py

--- Gg.py
+++ Gg.py
@@ -2,7 +2,6 @@ import telebot;
 bot = telebot.TeleBot('1988073321:AAEgxZLf5QeAavghZxzFvdyPo0uhPeS3U0I');
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-@bot.message_handler(content_types=['text', 'document', 'audio'])
 if message.text == "Привет":
     bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
 elif message.text == "/help":
